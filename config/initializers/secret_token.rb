dddce50ca8220d10e4fdb8371cab97352d51e8f2
David B <dialindo7@gmail.com>
2013-06-22 15:35:13 +0100

    Create a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dialindo::Application.config.secret_token = '848a527f9633191fdf7a4acb4e1144aa99826e3ca10864334143c34265611c2a3b324789a73736fdefad78034aa2fb5e07aac5e94cf555da68082edaf5c22b40'
