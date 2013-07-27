f352ffaa263a11a5bdc8a8776a8f635f72bc77fc
dabcoder <dbouchare@gmail.com>
2013-07-27 14:52:30 +0100

    Front end and forms
    Simple forms, added log in, log out, register and profile links

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :first_name, :last_name, :email, :profile_name, :password, :password_confirmation, :remember_me
+
+  has_many :statuses
+
+  def full_name
+  	first_name + " " + last_name
+  end
 end
